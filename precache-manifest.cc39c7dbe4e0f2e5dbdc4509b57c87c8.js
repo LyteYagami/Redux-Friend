6fdce2d34672b1a5c8bb4b2384200e17b1eea18c
Lyte_Yagami <nuh.satria@outlook.com>
2020-04-24 07:43:54 +0800

    Updates

---
+++ precache-manifest.cc39c7dbe4e0f2e5dbdc4509b57c87c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2f4bbace520b231bc6df8f3efb9116",
+    "url": "/Redux-Friend/index.html"
+  },
+  {
+    "revision": "be9b6d369af1e13cc6f9",
+    "url": "/Redux-Friend/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "5e2f6cdde59bee175f5d",
+    "url": "/Redux-Friend/static/css/main.4cf91217.chunk.css"
+  },
+  {
+    "revision": "be9b6d369af1e13cc6f9",
+    "url": "/Redux-Friend/static/js/2.f095a3df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Redux-Friend/static/js/2.f095a3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2f6cdde59bee175f5d",
+    "url": "/Redux-Friend/static/js/main.cbc09c83.chunk.js"
+  },
+  {
+    "revision": "0fdb010a0a9a05ade633",
+    "url": "/Redux-Friend/static/js/runtime-main.2bfcf87c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Redux-Friend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
